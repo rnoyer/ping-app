d3bc7c1ee9e64ac289aa43148af654987ed263f6
rnoyer <romain.noyer@gmail.com>
2026-05-30 19:10:52 +0200

    add api hello

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 8080;
 
+app.get("/", (req, res) => {
+  res.send("hellooow xD");
+});
+
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
